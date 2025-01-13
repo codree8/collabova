6f816d7a4a473a5ec7acde09d23cf603342fcc77
Igor Drecun <drecunigor8@gmail.com>
2025-01-14 00:21:12 +0100

    redirect images to s3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "proj-s3-images.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
