898ec133cc16e25bdee2bc561207aeabeb8d5de6
Igor Drecun <drecunigor8@gmail.com>
2025-01-14 01:58:58 +0100

    create user route post trigger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -10,6 +10,7 @@ import taskRoutes from "./routes/taskRoutes";
 import searchRoutes from "./routes/searchRoutes";
 import userRoutes from "./routes/userRoutes";
 import teamRoutes from "./routes/teamRoutes";
+
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -32,6 +33,9 @@ app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
+
+
+
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
